d52b467786a7f84f4a4339e14ad57c100679fedf
Marco Macaluso <marco.macaluso@incuto.com>
2022-06-10 15:11:50 +0100

    changed css

--- result.php
+++ result.php
@@ -27,101 +27,101 @@
     ?>
 
     <form action="index.php" method="POST" enctype="multipart/form-data" class="container">
-        <div class="container-fluid w-50 h-75 p-2 bg-info mt-2 rounded text-white">
-            <div class="row bg-info p-5">
 
-                <?php
+        <div class="row bg-info p-5">
 
-                try {
-                    $data_source = "mysql:host=$server;dbname=$database;charset=$charset";
-                    $pdo = new PDO($data_source, $username, $password);
-                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            <?php
 
-
-                    echo "Connection to DB successful";
+            try {
+                $data_source = "mysql:host=$server;dbname=$database;charset=$charset";
+                $pdo = new PDO($data_source, $username, $password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-                    if (empty($_POST['user-name']) || is_numeric($_POST['user-name'])) {
-                        throw new \Exception('Error: Please enter a valid name.');
-                    }
-                    if (empty($_POST['user-email']) || (!filter_var($_POST['user-email'], FILTER_VALIDATE_EMAIL))) {
-                        throw new \Exception('Error: Please enter a valid email.');
-                    }
-                    if (empty($_POST['dropdown'])) {
-                        throw new \Exception('Error: No issue type selected');
-                    }
-                    if (empty($_POST['notes'])) {
-                        throw new \Exception('Error: Please provide a short description of the reason for your email.');
-                    }
+                echo "Connection to DB successful";
 
 
-                    $name = htmlspecialchars($_POST['user-name']);
-                    $email = htmlspecialchars($_POST['user-email']);
-                    $issue = htmlspecialchars($_POST['dropdown']);
-                    $notes = htmlspecialchars(strip_tags($_POST['notes']));
-
-                ?>
-
-                    <div class="col-md-3">
-                        <label>
-                            Name
-                        </label>
-                        <input class="form-control" type="text" name="user-name" value="<?= $name ?>">
-                    </div>
-                    <div class="col-md-3">
-                        <label>
-                            E-mail
-                        </label>
-                        <input class="form-control" type="text" name="user-email" value="<?= $email ?>">
-                    </div>
-                    <div class="col-md-3 pt-5">
-                        <label>
-                            Issue type
-                        </label>
-                        <select name="dropdown" id="dropdown">
-                            <option value="" hidden>Select issue</option>
-                            <?php
-                            // cycles through all arr elements and checks whether $_POSTdropdown is equal to $id (which is populated by the array)
-                            foreach ($issue_type as $id => $display) {
-                                echo '<option ' . (($_POST['dropdown'] == $id) ? "selected" : "") . ' value="' . $id . '" >' . $display . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="col-md-3">
-                        <label>
-                            Notes
-                        </label>
-                        <input class="form-control" type="text" id="notes" name="notes" value="<?= $notes ?>">
-                    </div>
-
-                <?php
-
-                    $data = [
-                        'customerName' => $name,
-                        'customerEmail' => $email,
-                        'issueType' => $issue,
-                        'issueNotes' => $notes,
-                    ];
-                    $sql = "INSERT INTO enquiries (customerName, customerEmail, issueType, issueNotes) 
-                            VALUES (:customerName, :customerEmail, :issueType, :issueNotes)";
-
-                    $statement = $pdo->prepare($sql);
-
-                    // column with a : before indiciate a named placeholder
-                    $statement->execute(array(':customerName' => $name, 'customerEmail' => $email, 'issueType' => $issue, 'issueNotes' => $notes));
-                } catch (\Exception $Exception) {
-                    echo $Exception->getMessage();
+                if (empty($_POST['user-name']) || is_numeric($_POST['user-name'])) {
+                    throw new \Exception('Error: Please enter a valid name.');
+                }
+                if (empty($_POST['user-email']) || (!filter_var($_POST['user-email'], FILTER_VALIDATE_EMAIL))) {
+                    throw new \Exception('Error: Please enter a valid email.');
+                }
+                if (empty($_POST['dropdown'])) {
+                    throw new \Exception('Error: No issue type selected');
+                }
+                if (empty($_POST['notes'])) {
+                    throw new \Exception('Error: Please provide a short description of the reason for your email.');
                 }
 
 
-                ?>
+                $name = htmlspecialchars($_POST['user-name']);
+                $email = htmlspecialchars($_POST['user-email']);
+                $issue = htmlspecialchars($_POST['dropdown']);
+                $notes = htmlspecialchars(strip_tags($_POST['notes']));
 
-                <div class="col-md-6 text-left">
-                    <button class="btn btn-success" type="submit" id="submit-btn">Resubmit Form</button>
+            ?>
+
+                <div class="col-md-3">
+                    <label>
+                        Name
+                    </label>
+                    <input class="form-control" type="text" name="user-name" value="<?= $name ?>">
                 </div>
+                <div class="col-md-3">
+                    <label>
+                        E-mail
+                    </label>
+                    <input class="form-control" type="text" name="user-email" value="<?= $email ?>">
+                </div>
+                <div class="col-md-3 pt-5">
+                    <label>
+                        Issue type
+                    </label>
+                    <select name="dropdown" id="dropdown">
+                        <option value="" hidden>Select issue</option>
+                        <?php
+                        // cycles through all arr elements and checks whether $_POSTdropdown is equal to $id (which is populated by the array)
+                        foreach ($issue_type as $id => $display) {
+                            echo '<option ' . (($_POST['dropdown'] == $id) ? "selected" : "") . ' value="' . $id . '" >' . $display . '</option>';
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="col-md-3">
+                    <label>
+                        Notes
+                    </label>
+                    <input class="form-control" type="text" id="notes" name="notes" value="<?= $notes ?>">
+                </div>
+
+            <?php
+
+                $data = [
+                    'customerName' => $name,
+                    'customerEmail' => $email,
+                    'issueType' => $issue,
+                    'issueNotes' => $notes,
+                ];
+                $sql = "INSERT INTO enquiries (customerName, customerEmail, issueType, issueNotes) 
+                            VALUES (:customerName, :customerEmail, :issueType, :issueNotes)";
+
+                $statement = $pdo->prepare($sql);
+
+                // column with a : before indiciate a named placeholder
+                $statement->execute(array(':customerName' => $name, 'customerEmail' => $email, 'issueType' => $issue, 'issueNotes' => $notes));
+            } catch (\Exception $Exception) {
+                echo $Exception->getMessage();
+            }
+
+
+            ?>
+
+            <div class="col-md-6 text-left">
+                <button class="btn btn-success" type="submit" id="submit-btn">Resubmit Form</button>
             </div>
         </div>
+
     </form>
 </body>
 
